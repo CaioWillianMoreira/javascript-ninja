8d05396e03453a47a63559c480a52d120e219124
Caio Willian <cwillian40@hotmail.com>
2020-08-22 01:21:31 -0300

    [003] operadores de igualdade ou relacionais

---
+++ semana-01/03-operadores-igualdade-ou-relacionais.js
@@ -0,0 +1,39 @@
+/*
+  Operadores de igualdade ou relacionais
+*/
+
+// == igual a
+// != diferente de
+
+// operador de atribuição
+var x = 0;
+console.log(x);
+
+/*
+  Operadores de igualdade servem pra testar se
+  valores são igual a outros ou diferente de outros
+*/
+
+// operador de comparação
+console.log(1 == 1);
+
+// operador diferente
+console.log(1 != 2);
+
+// Operador de igualdade e do mesmo tipo
+console.log('1' === 1);
+
+// Operador diferente, mais do mesmo tipo
+console.log('2' !== 2);
+
+// maior que
+console.log(1 > 2);
+
+// menor que
+console.log(3 < 2);
+
+// maior ou igual a
+console.log(5 >= 5);
+
+// menor ou igual a
+console.log(6 <= 2);
