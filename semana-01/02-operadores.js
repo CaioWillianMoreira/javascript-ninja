f86736de889d8c8942ec6fc6ef9dd259ff9d653f
Caio Willian <cwillian40@hotmail.com>
2020-08-22 00:55:38 -0300

    [002] Operadores aritiméticos

---
+++ semana-01/02-operadores.js
@@ -0,0 +1,54 @@
+/*
+Operadores aritiméticos
++ Adição
+- Subtração
+* Multiplicação
+/ Divisão
+*/
+
+// soma
+console.log(1+2);
+
+// subtração
+console.log(4-2);
+
+// multiplicação
+console.log(2*2);
+
+// divisão
+console.log(1/2);
+
+/*
+  Operadores aritiméticos abreviados
+*/
+
+// exemplo
+var soma = 10 + 10;
+console.log(soma);
+
+// exemplo operadores abreviados
+console.log(soma = soma + 1);
+// ou
+console.log(soma = soma - 1);
+
+// operador pos incremento
+console.log(soma++);
+
+// operador pre incremento
+console.log(++soma);
+
+/*
+  Outros formatos de operadores abreviados
+*/
+
+// atribue o valor atual mais 20
+console.log(soma += 20);
+
+// pega o valor atual e diminue 20
+console.log(soma -= 20);
+
+// pega o valor atual e divide por 2
+console.log(soma /= 2);
+
+// pega o valor atual e multiplica por 5
+console.log(soma *= 5);
